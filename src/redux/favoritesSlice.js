6724dcfdad01105db65ea306bc8dae2d35c96374
Rudy-X7 <rudraksh0301.g@gmail.com>
2025-05-23 01:39:30 +0530

    Update favoritesSlice.js

--- src/redux/favoritesSlice.js
+++ src/redux/favoritesSlice.js
@@ -1,14 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  favoriterecipes: [], // Updated to handle favorite articles
+  favoriterecipes: [], // List of favorite recipes
 };
 
 const favoritesSlice = createSlice({
   name: "favorites",
   initialState,
   reducers: {
-   
+    toggleFavorite: (state, action) => {
+      const existingIndex = state.favoriterecipes.findIndex(
+        (recipe) => recipe.idFood === action.payload.idFood
+      );
+
+      if (existingIndex >= 0) {
+        // Recipe already in favorites, remove it
+        state.favoriterecipes.splice(existingIndex, 1);
+      } else {
+        // Recipe not in favorites, add it
+        state.favoriterecipes.push(action.payload);
+      }
+    },
   },
 });
 
